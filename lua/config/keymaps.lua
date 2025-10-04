73a8c47f1e62d7f2bca44f9d91b105560b5498c1
Berk Odeon <berkodeon@Berks-MacBook-Pro.fritz.box>
2025-10-04 22:36:07 +0200

    Add lsp with fuzzy search plugin

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,9 @@
 vim.keymap.set("n", "-", "<cmd>Oil --float<CR>", {desc="Open Parent Directory in Oil"})
 
 -- neo-tree
-vim.keymap.set('n', '<C-e>', '<Cmd>Neotree toggle<CR>')
+vim.keymap.set('n', '<C-e>', '<Cmd>Neotree toggle<CR>', {desc="Open/close file tree at navbar"})
+
+
+vim.keymap.set('n', "gl", function() vim.diagnostic.open_float() end, {desc="Open Diagnostics in Float"})
 
 
