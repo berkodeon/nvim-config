da548e2ed247f7ca28f0c9b5ac1c5bb7121538f4
Berk Odeon <berkodeon@Berks-MacBook-Pro.fritz.box>
2025-10-05 10:57:15 +0200

    Adding code completion

--- README.md
+++ README.md
@@ -2,8 +2,16 @@
 From environment to environment, I realized I reconfigure my neovim config and I wanted to merge all my personal configuration by creating this repo while refactoring and using latest versions.
 
 # Next TODOs
-- [] Paranthesis matcher
-- [] Add Pyton LSP
-- [] Add Rust LSP
+- [X] Paranthesis matcher
+- [X] Add Pyton LSP
+- [X] Add Rust LSP
 - [] Cloak
 - [] Zenmode
+
+
+# In case we need to install another auto-completion plugin, here is the source:
+https://cmp.saghen.dev/configuration/sources#community-sources
+
+# After installation
+- Run `:checkhealth`!
+
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,10 +1,15 @@
 -- OIL
-vim.keymap.set("n", "-", "<cmd>Oil --float<CR>", {desc="Open Parent Directory in Oil"})
+vim.keymap.set("n", "-", "<cmd>Oil --float<CR>", { desc = "Open Parent Directory in Oil" })
 
 -- neo-tree
-vim.keymap.set('n', '<C-e>', '<Cmd>Neotree toggle<CR>', {desc="Open/close file tree at navbar"})
+vim.keymap.set("n", "<C-e>", "<Cmd>Neotree toggle<CR>", { desc = "Open/close file tree at navbar" })
 
+-- diagnostics
+vim.keymap.set("n", "gl", function()
+    vim.diagnostic.open_float()
+end, { desc = "Open Diagnostics in Float" })
 
-vim.keymap.set('n', "gl", function() vim.diagnostic.open_float() end, {desc="Open Diagnostics in Float"})
-
-
+-- formatter
+vim.keymap.set("n", "<leader>cf", function()
+    require("conform").format({ lsp_format = "fallback" })
+end, { desc = "[C]ode [F]ormat" })
