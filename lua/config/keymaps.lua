8222fef8c9a66a76e34515e02c99127d6279fba7
Berk Odeon <berkodeon@Berks-MacBook-Pro.local>
2026-03-04 16:40:04 +0100

    Adding overseer to have task list to run/debug an app

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -60,3 +60,9 @@ end, { desc = "Summary" })
 vim.keymap.set("n", "<leader>tv", "<cmd>TestVisit<CR>", { desc = "Visit" })
 
 vim.keymap.set("n", "<leader>tx", "<cmd>TestSuite<CR>", { desc = "Suite" })
+
+-- overseer
+vim.keymap.set("n", "<leader>or", "<cmd>OverseerRun<CR>", { desc = "[O]verseer [R]un" })
+vim.keymap.set("n", "<leader>oo", "<cmd>OverseerToggle<CR>", { desc = "[O]verseer [O]pen" })
+vim.keymap.set("n", "<leader>oa", "<cmd>OverseerQuickAction<CR>", { desc = "[O]verseer [A]ction" })
+vim.keymap.set("n", "<leader>oi", "<cmd>OverseerInfo<CR>", { desc = "[O]verseer [I]nfo" })
---
+++ lua/plugins/overseer.lua
@@ -0,0 +1,13 @@
+return {
+  "stevearc/overseer.nvim",
+  ---@module 'overseer'
+  ---@type overseer.SetupOpts
+  opts = {
+    task_list = {
+      direction = "bottom",
+      min_height = 25,
+      max_height = 25,
+      default_detail = 1,
+    },
+  },
+}
